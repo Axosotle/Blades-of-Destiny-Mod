1b0affb287880146b8bb5db1339dd85bad74340d
kititnik <kititnik@gmail.com>
2024-06-21 14:18:27 +0300

    Add cursed dagger

---
+++ src/main/java/net/axokit/blades_of_destiny/BODMod.java
@@ -0,0 +1,48 @@
+package net.axokit.blades_of_destiny;
+
+import net.axokit.blades_of_destiny.item.ModItems;
+import com.mojang.logging.LogUtils;
+import net.minecraftforge.api.distmarker.Dist;
+import net.minecraftforge.common.MinecraftForge;
+import net.minecraftforge.event.server.ServerStartingEvent;
+import net.minecraftforge.eventbus.api.IEventBus;
+import net.minecraftforge.eventbus.api.SubscribeEvent;
+import net.minecraftforge.fml.common.Mod;
+import net.minecraftforge.fml.event.lifecycle.FMLClientSetupEvent;
+import net.minecraftforge.fml.event.lifecycle.FMLCommonSetupEvent;
+import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
+import org.slf4j.Logger;
+
+@Mod(BODMod.MOD_ID)
+public class BODMod {
+    public static final String MOD_ID = "blades_of_destiny";
+    public static final Logger LOGGER = LogUtils.getLogger();
+
+    public BODMod() {
+        IEventBus modEventBus = FMLJavaModLoadingContext.get().getModEventBus();
+
+        ModItems.register(modEventBus);
+
+        modEventBus.addListener(this::commonSetup);
+
+        MinecraftForge.EVENT_BUS.register(this);
+    }
+
+    private void commonSetup(final FMLCommonSetupEvent event) {
+
+    }
+
+
+    @SubscribeEvent
+    public void onServerStarting(ServerStartingEvent event) {
+
+    }
+
+    @Mod.EventBusSubscriber(modid = MOD_ID, bus = Mod.EventBusSubscriber.Bus.MOD, value = Dist.CLIENT)
+    public static class ClientModEvents {
+        @SubscribeEvent
+        public static void onClientSetup(FMLClientSetupEvent event) {
+
+        }
+    }
+}
---
+++ src/main/java/net/axokit/blades_of_destiny/item/ModItems.java
@@ -0,0 +1,28 @@
+package net.axokit.blades_of_destiny.item;
+
+import net.axokit.blades_of_destiny.BODMod;
+import net.minecraft.world.item.Item;
+import net.minecraft.world.item.SwordItem;
+import net.minecraft.world.item.Tiers;
+import net.minecraftforge.eventbus.api.IEventBus;
+import net.minecraftforge.registries.DeferredRegister;
+import net.minecraftforge.registries.ForgeRegistries;
+import net.minecraftforge.registries.RegistryObject;
+
+public class ModItems {
+    public  static final DeferredRegister<Item> ITEMS =
+            DeferredRegister.create(ForgeRegistries.ITEMS, BODMod.MOD_ID);
+
+    public  static final RegistryObject<Item> CURSED_DAGGER = ITEMS.register("cursed_dagger",
+            () -> new SwordItem(
+                    Tiers.DIAMOND,
+                    7,
+                    2.5f,
+                    new Item.Properties()
+            ));
+
+    public  static void register(IEventBus eventBus)
+    {
+        ITEMS.register(eventBus);
+    }
+}
